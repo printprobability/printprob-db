4469adf502d3c1faa1603ccc7c42ff43be43c829
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-07-16 14:52:49 -0400

    backup script

---
+++ src/bkp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+/usr/local/bin/docker-compose -f /home/mlincoln/db/docker-compose.yml --project-directory /home/mlincoln/db/ exec postgres pg_dump -U app pp > /home/mlincoln/bkp/bkp.sql
+
+cd /home/mlincoln/bkp
+/usr/bin/git add .
+DATE=`date +%Y-%m-%d`
+/usr/bin/git commit -m "incremental backup $DATE"
