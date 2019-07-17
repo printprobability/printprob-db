f046d2930a1e92584038cc69fba027691028ee90
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-07-17 12:17:41 -0400

    echo backup commands as they are made

--- src/bkp.sh
+++ src/bkp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 /usr/local/bin/docker-compose -f /home/mlincoln/db/docker-compose.yml --project-directory /home/mlincoln/db/ exec postgres pg_dump -U app pp > /home/mlincoln/bkp/bkp.sql
 
