6029a732655955bbf5bbf06b212632e84989f341
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-07-18 10:42:41 -0400

    backup with a dumpall command
    preserves roles

--- src/bkp.sh
+++ src/bkp.sh
@@ -3,7 +3,9 @@
 set -e
 set -x
 
-/usr/local/bin/docker-compose -f /home/mlincoln/db/docker-compose.yml --project-directory /home/mlincoln/db/ exec postgres pg_dump -U app pp > /home/mlincoln/bkp/bkp.sql
+source /home/mlincoln/db/.env
+
+/usr/local/bin/docker-compose -f /home/mlincoln/db/docker-compose.yml --project-directory /home/mlincoln/db/ exec -t postgres pg_dumpall -U app > /home/mlincoln/bkp/bkp.sql
 
 cd /home/mlincoln/bkp
 /usr/bin/git add .
