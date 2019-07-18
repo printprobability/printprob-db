dedba7ec63b3c8677f9473fdca2beebd0db97a95
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-07-18 11:02:37 -0400

    dont allocate TTY on backup

--- src/bkp.sh
+++ src/bkp.sh
@@ -5,7 +5,7 @@ set -x
 
 source /home/mlincoln/db/.env
 
-/usr/local/bin/docker-compose -f /home/mlincoln/db/docker-compose.yml --project-directory /home/mlincoln/db/ exec -t postgres pg_dumpall -U app > /home/mlincoln/bkp/bkp.sql
+/usr/local/bin/docker-compose -f /home/mlincoln/db/docker-compose.yml --project-directory /home/mlincoln/db/ exec -T postgres pg_dumpall -U app > /home/mlincoln/bkp/bkp.sql
 
 cd /home/mlincoln/bkp
 /usr/bin/git add .
