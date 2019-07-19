8be86a225d78136940e876299cc3fb0493b7d4c0
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-07-19 18:36:56 -0400

    postgres loop to get readonly permissions for pp tables

--- src/readonly.sql
+++ src/readonly.sql
@@ -1,4 +1,12 @@
 -- SQL to create 'readonly' role on PP Postgres tables for remote analysis
 
-CREATE ROLE readonly LOGIN;
-SELECT 'GRANT SELECT ON public.' || table_name || ' TO readonly;' FROM information_schema.tables WHERE table_name LIKE 'pp%';
+CREATE ROLE readonly;
+DO $$
+DECLARE
+  rec record;
+BEGIN
+  FOR rec IN SELECT table_name FROM information_schema.tables WHERE table_name LIKE 'pp%'
+  LOOP
+    EXECUTE 'GRANT SELECT ON public.' || quote_ident(rec.table_name) || ' TO readonly;';
+  END LOOP;
+END $$;
