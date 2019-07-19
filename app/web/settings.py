056d629aa173b00948a8347e75a322e55d5c3b18
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-07-19 08:50:18 -0400

    revert to yasg
    The default doc formatting has nested/grouped descriptions, much nicer

--- app/web/settings.py
+++ app/web/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
+    "drf_yasg",
     "rest_framework.authtoken",
     "django_filters",
 ]
