067399a92f9c6332a5158659be61ebef0cc23d83
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-09-22 11:19:55 -0400

    display char count on homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <b-jumbotron header="Print Viewer" lead class="my-2">
+  <b-jumbotron header="Print Viewer" :lead="display_count" class="my-2">
     <p>
-      An interactive viewer for Carnegie Mellon University's
+      An interactive viewer and editor for Carnegie Mellon University's
       <em>Print and Probability</em> project to create a dictionary of distinctive early modern type.
     </p>
     <b-button variant="primary" href="http://localhost/api-authlogin">Log in</b-button>
@@ -9,7 +9,45 @@
 </template>
 
 <script>
+import { HTTP } from "../main";
 export default {
-  name: "PageImage"
+  name: "Home",
+  data() {
+    return {
+      nchars: null
+    };
+  },
+  computed: {
+    display_count: function() {
+      if (!!this.nchars) {
+        return (
+          "Indexing " +
+          this.nchars.toLocaleString() +
+          " characters and counting..."
+        );
+      } else {
+        return "";
+      }
+    }
+  },
+  methods: {
+    get_nchars: function() {
+      return HTTP.get("/characters/", {
+        params: {
+          limit: 1
+        }
+      }).then(
+        response => {
+          this.nchars = response.data.count;
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
+  },
+  mounted: function() {
+    this.get_nchars();
+  }
 };
 </script>
