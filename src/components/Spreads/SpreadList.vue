14daebb24f29b1abff15db9e09e73310e851585d
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-09-25 08:24:12 -0400

    card columns make more consistent spread lists

--- src/components/Spreads/SpreadList.vue
+++ src/components/Spreads/SpreadList.vue
@@ -1,16 +1,11 @@
 <template>
-  <div class="card">
-    <div
-      class="card-header d-flex justify-content-between align-items-center"
-    >{{ spreads.length }} Spreads</div>
-    <div class="card-body d-flex flex-wrap justify-content-between">
-      <SpreadImage
-        v-for="spread in spreads"
-        :spread="spread"
-        :key="spread.id"
-        :header="String(spread.sequence)"
-      />
-    </div>
+  <div class="card-columns">
+    <SpreadImage
+      v-for="spread in spreads"
+      :spread="spread"
+      :key="spread.id"
+      :header="String(spread.sequence)"
+    />
   </div>
 </template>
 
