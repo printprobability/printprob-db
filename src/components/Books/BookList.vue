eed41fd0cbe6b04ea3d319ed7f031b3e2e4bb58f
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-10-19 10:39:34 -0400

    compact the book search options

--- src/components/Books/BookList.vue
+++ src/components/Books/BookList.vue
@@ -3,82 +3,90 @@
     <div class="card my-2">
       <div class="card-header">Filter books</div>
       <div class="card-body">
-        <h5>EEBO metadata</h5>
         <b-row>
-          <div class="col-4">
-            <b-form-group
-              id="publisher-group"
-              label="Publisher"
-              label-for="publisher-input"
-              description="Partial publisher name (case insensitive)"
-            >
-              <b-form-input id="publisher-input" v-model="publisher_search" placeholder="overton" />
-            </b-form-group>
-          </div>
-          <div class="col-4">
-            <b-form-group
-              id="title-group"
-              label-for="title-input"
-              label="Title"
-              description="Search by partial title (case insensitive)"
-            >
-              <b-form-input id="title-input" v-model="title_search" placeholder="nine arguments" />
-            </b-form-group>
-          </div>
-          <div class="col-4">
-            <b-form-group
-              id="author-group"
-              label-for="author-input"
-              label="Author"
-              description="Search by partial author (case insensitive)"
-            >
-              <b-form-input id="author-input" v-model="author_search" placeholder="milton" />
-            </b-form-group>
-          </div>
-        </b-row>
-
-        <h5>P&P metadata</h5>
-        <b-row>
-          <b-col cols="4">
-            <b-form-group
-              id="pp-publisher-group"
-              label="Search by partial publisher (as assigned by P&P)"
-            >
-              <b-form-input
-                id="pp-publisher-input"
-                v-model="pp_publisher_search"
-                placeholder="simmons"
-              />
-            </b-form-group>
+          <b-col col lg="6" class="border-right p-3">
+            <h5>EEBO metadata</h5>
+            <b-form-row>
+              <b-col col md="6">
+                <b-form-group
+                  id="publisher-group"
+                  label="Publisher"
+                  label-for="publisher-input"
+                  description="Partial publisher name (case insensitive)"
+                >
+                  <b-form-input
+                    id="publisher-input"
+                    v-model="publisher_search"
+                    placeholder="overton"
+                  />
+                </b-form-group>
+                <b-form-group
+                  id="title-group"
+                  label-for="title-input"
+                  label="Title"
+                  description="Search by partial title (case insensitive)"
+                >
+                  <b-form-input
+                    id="title-input"
+                    v-model="title_search"
+                    placeholder="nine arguments"
+                  />
+                </b-form-group>
+              </b-col>
+              <b-col col md="6">
+                <b-form-group
+                  id="author-group"
+                  label-for="author-input"
+                  label="Author"
+                  description="Search by partial author (case insensitive)"
+                >
+                  <b-form-input id="author-input" v-model="author_search" placeholder="milton" />
+                </b-form-group>
+              </b-col>
+            </b-form-row>
           </b-col>
-          <b-col cols="4">
-            <b-form-group id="book-image-group" label="Only show books with images">
-              <b-form-checkbox id="book-image-input" v-model="has_images" />
-            </b-form-group>
-          </b-col>
-          <b-col cols="4">
-            <b-form-group
-              id="date-range-group"
-              label="Books published between"
-              description="Only books whose dates are wholly within the specified range"
-            >
-              <b-form inline>
-                <b-form-input
-                  class="mx-2"
-                  size="sm"
-                  id="year-input-early"
-                  type="date"
-                  v-model="year_early"
-                />and
-                <b-form-input
-                  class="mx-2"
-                  size="sm"
-                  id="year-input-late"
-                  type="date"
-                  v-model="year_late"
-                />
-              </b-form>
-            </b-form-group>
+          <b-col col lg="6" class="p-3">
+            <h5>P&P metadata</h5>
+            <b-form-row>
+              <b-col col md="6">
+                <b-form-group
+                  id="pp-publisher-group"
+                  label="Publisher"
+                  description="Search by partial publisher (as assigned by P&P)"
+                >
+                  <b-form-input
+                    id="pp-publisher-input"
+                    v-model="pp_publisher_search"
+                    placeholder="simmons"
+                  />
+                </b-form-group>
+                <b-form-group id="book-image-group" label="Only show books with images">
+                  <b-form-checkbox id="book-image-input" v-model="has_images" />
+                </b-form-group>
+              </b-col>
+              <b-col col md="6">
+                <b-form-group
+                  id="date-range-group"
+                  label="Books published between"
+                  description="Only books whose dates are wholly within the specified range"
+                >
+                  <b-form inline>
+                    <b-form-input
+                      class="mx-2"
+                      id="year-input-early"
+                      type="date"
+                      v-model="year_early"
+                    />and
+                    <b-form-input
+                      class="mx-2"
+                      id="year-input-late"
+                      type="date"
+                      v-model="year_late"
+                    />
+                  </b-form>
+                </b-form-group>
+              </b-col>
+            </b-form-row>
           </b-col>
         </b-row>
       </div>
