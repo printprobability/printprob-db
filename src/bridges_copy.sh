4fbf0cdc85eacb1d6cf8a1413c279bd6cc1e54fc
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-11-20 17:16:51 -0500

    write a  book-specific file transfer txt

--- src/bridges_copy.sh
+++ src/bridges_copy.sh
@@ -6,9 +6,9 @@ PP_TOKEN=`cat ~/token.txt`
 
 # Generate list of page files to download
 cd /pylon5/hm560ip/mpwillia/line_extractions/complete/$1
-find pages/ -regextype posix-extended -regex '.+([0-9]{3}|r)\.tif' > ~/imagefiles.txt
+find pages/ -regextype posix-extended -regex '.+([0-9]{3}|r)\.tif' > ~/$1_imagefiles.txt
 cd ~
 
-rsync -avh --files-from ~/imagefiles.txt /pylon5/hm560ip/mpwillia/line_extractions/complete/$1/ bridges@printprobdb.library.cmu.edu:/vol/images/mpwillia/line_extractions/complete/$1/
+rsync -avh --files-from ~/$1_imagefiles.txt /pylon5/hm560ip/mpwillia/line_extractions/complete/$1/ bridges@printprobdb.library.cmu.edu:/vol/images/mpwillia/line_extractions/complete/$1/
 
 python3 backlog.py -e https://printprobdb.library.cmu.edu/api/ -t $PP_TOKEN -r /pylon5/hm560ip/ $1
