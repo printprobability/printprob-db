1e932aefcc5090c92189f73beb9cd49da3ae9d49
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-11-20 15:17:31 -0500

    add card w/ char info to CharacterDetail

--- src/components/Characters/CharacterDetail.vue
+++ src/components/Characters/CharacterDetail.vue
@@ -1,8 +1,30 @@
 <template>
-  <div class="container-flex" v-if="!!character">
-    <h3>Character {{ character.label }}</h3>
-    <AnnotatedImage :id="this.character.id" :image_info_url="image_info_url" :overlay="annotation" />
-  </div>
+  <b-row v-if="!!character">
+    <b-col cols="3">
+      <b-card header="Character detail" class="m-2">
+        <p>
+          Book:
+          <router-link
+            :to="{name: 'BookDetailView', params: {id: this.character.book.id}}"
+          >{{ this.character.book.label }}</router-link>
+        </p>
+        <p>Location: Spread {{ this.character.spread.sequence }}, page {{ this.character.page.side }}, line {{ this.character.line.sequence }}, position {{ this.character.sequence }}</p>
+        <p>Assigned class: {{ this.character.character_class }} ({{ this.character.class_probability}})</p>
+        <p>Human character class: {{ this.character.human_character_class }}</p>
+        <p>
+          Character id:
+          <code>{{ this.character.id }}</code>
+        </p>
+      </b-card>
+    </b-col>
+    <b-col cols="9">
+      <AnnotatedImage
+        :id="this.character.id"
+        :image_info_url="image_info_url"
+        :overlay="annotation"
+      />
+    </b-col>
+  </b-row>
 </template>
 
 <script>
@@ -18,9 +40,19 @@ export default {
     id: String
   },
   data() {
-    return {
-      character: null
-    };
+    return {};
+  },
+  asyncComputed: {
+    character() {
+      return HTTP.get("/characters/" + this.id + "/").then(
+        response => {
+          return response.data;
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
   },
   computed: {
     annotation() {
@@ -29,21 +61,6 @@ export default {
     image_info_url() {
       return this.character.page.image.iiif_base + "/info.json";
     }
-  },
-  methods: {
-    get_character: function(id) {
-      return HTTP.get("/characters/" + id + "/").then(
-        response => {
-          this.character = response.data;
-        },
-        error => {
-          console.log(error);
-        }
-      );
-    }
-  },
-  mounted() {
-    this.get_character(this.id);
   }
 };
 </script>
