c105f50d9a4f7f3844844d7dfd1830a2a8abf841
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-12-28 13:09:10 -0500

    don't try to display book/letter counts anymore

--- vue/src/components/Home.vue
+++ vue/src/components/Home.vue
@@ -6,65 +6,17 @@
     </template>
     <hr class="my-4" />
     <b-button v-if="!logged_in" :href="$APIConstants.API_LOGIN" variant="primary">Log In</b-button>
-    <template v-else>
-      <p v-show="!!display_count">{{ display_count }}</p>
-    </template>
   </b-jumbotron>
 </template>
 
 <script>
-import { HTTP } from "../main";
 export default {
   name: "Home",
   props: {
     logged_in: Boolean
   },
   data() {
-    return {
-      nchars: null,
-      nbooks: null
-    };
-  },
-  computed: {
-    display_count: function() {
-      if (!!this.nchars && !!this.nbooks) {
-        return (
-          "Indexing " +
-          this.nbooks.toLocaleString() +
-          " books with about " +
-          this.nchars.toLocaleString() +
-          " characters and counting..."
-        );
-      } else {
-        return null;
-      }
-    }
-  },
-  methods: {
-    get_nchars: function() {
-      return HTTP.get("/lines/count/").then(
-        response => {
-          this.nchars = response.data.count * 80;
-        },
-        error => {
-          console.log(error);
-        }
-      );
-    },
-    get_nbooks: function() {
-      return HTTP.get("/books/count/").then(
-        response => {
-          this.nbooks = response.data.count;
-        },
-        error => {
-          console.log(error);
-        }
-      );
-    }
-  },
-  mounted: function() {
-    this.get_nchars();
-    this.get_nbooks();
+    return {};
   }
 };
 </script>
