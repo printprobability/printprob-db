028b02ea4d607489e1ab29825475381037d6c4ee
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-06-29 16:43:15 -0400

    add script version to run models

--- rest/app/pp/models.py
+++ rest/app/pp/models.py
@@ -28,6 +28,7 @@ class Run(uuidModel):
         "Book", on_delete=models.CASCADE, related_name="%(class)ss"
     )
     date_started = models.DateTimeField(auto_now_add=True)
+    script_version = models.CharField(max_length=200, default="", blank=True)
 
     class Meta:
         abstract = True
