a2d45d0caefb4bd9d8e48db76b50ef1d01c76ad2
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-09-16 12:55:43 -0400

    increase max length again

--- rest/app/pp/models.py
+++ rest/app/pp/models.py
@@ -415,7 +415,7 @@ class CharacterClass(models.Model):
         primary_key=True, max_length=50, help_text="Unique Ocular identifier",
     )
     label = models.CharField(
-        max_length=15, default="", help_text="Human-readable label"
+        max_length=50, default="", help_text="Human-readable label"
     )
     group = models.CharField(
         max_length=2, choices=CHARACTER_GROUPS, default=LOWERCASE, db_index=True
