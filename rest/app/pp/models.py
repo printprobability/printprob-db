9646cf34404dd77fb066de5950d4b2105058c3de
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-09-23 15:37:24 -0400

    allow UUID to be set by user

--- rest/app/pp/models.py
+++ rest/app/pp/models.py
@@ -8,7 +8,7 @@ import math
 
 
 class uuidModel(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
     label = models.CharField(max_length=200, default="", editable=False, blank=True)
 
     class Meta:
