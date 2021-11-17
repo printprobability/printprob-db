309de9aa74965bf834c62e7c3daf57605aa1ac31
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2021-11-17 13:57:53 -0500

    add cache flag to refresh_labels

--- rest/app/pp/management/commands/refresh_labels.py
+++ rest/app/pp/management/commands/refresh_labels.py
@@ -7,20 +7,28 @@ from tqdm import tqdm
 class Command(BaseCommand):
     help = "Refresh all materialized labels for a Page, Line, Character, etc."
 
+    def add_arguments(self, parser):
+        parser.add_argument("--cached", action="store_true", help="Only refresh objects with no labels")
+
     def handle(self, *args, **options):
         labelled_models = [
-            models.Image,
             models.Book,
             models.PageRun,
             models.LineRun,
             models.CharacterRun,
             models.Spread,
-            models.Line,
             models.Page,
+            models.Line,
             models.Character,
         ]
 
+        use_cache = options["cached"]
+
         for m in labelled_models:
             print(m)
-            for i in tqdm(m.objects.all()):
+            if (use_cache):
+                qs = m.objects.filter(label="")
+            else:
+                qs = m.objects.all()
+            for i in tqdm(qs, total=qs.count()):
                 i.save()
