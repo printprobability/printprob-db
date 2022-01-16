4a5f5107f97999a33bbf15e41b6850925259b572
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2022-01-16 16:15:24 -0500

    collect all pages and custom tiles into JSON for iiiif-zero conversion

--- rest/app/pp/management/commands/json_dump.py
+++ rest/app/pp/management/commands/json_dump.py
@@ -26,7 +26,14 @@ class Command(BaseCommand):
         OUTPUT_PATH = "serialized_json"
 
         groupings = models.CharacterGrouping.objects.all()
-        chars = models.Character.objects.filter(charactergroupings__isnull=False)
+        chars = models.Character.objects.filter(
+            charactergroupings__isnull=False
+        ).order_by(
+            "created_by_run__book__id",
+            "line__page__sequence",
+            "line__sequence",
+            "sequence",
+        )
         books = models.Book.objects.filter(
             characterruns__characters__in=chars
         ).distinct()
@@ -61,7 +68,7 @@ class Command(BaseCommand):
                     serializers.CharacterDetailSerializer(
                         instance=c, context={"request": None}
                     ).data
-                    for c in chars.filter(character_class=cc).distinct()
+                    for c in chars.filter(character_class=cc)
                 ],
             }
             all_class_data.append(cc_ser)
@@ -77,7 +84,7 @@ class Command(BaseCommand):
                 serializers.CharacterDetailSerializer(
                     instance=c, context={"request": None}
                 ).data
-                for c in chars.filter(created_by_run__book=book).distinct()
+                for c in chars.filter(created_by_run__book=book)
             ]
             all_book_data.append(bk_ser)
         with open(f"serialized_json/books.json", "w") as dumpfile:
@@ -92,8 +99,40 @@ class Command(BaseCommand):
                 serializers.CharacterDetailSerializer(
                     instance=c, context={"request": None}
                 ).data
-                for c in grouping.characters.all()
+                for c in chars.filter(charactergroupings=grouping)
             ]
             all_grouping_data.append(group_ser)
         with open(f"serialized_json/groupings.json", "w") as dumpfile:
             json.dump(all_grouping_data, dumpfile, cls=UUIDEncoder, indent=2)
+
+        # create full list of images
+        all_images = {}
+        for c in all_char_data:
+            page_url = c["page"]["image"]["iiif_base"]
+            identifier = c["page"]["id"]
+            if identifier not in all_images:
+                all_images[identifier] = {
+                    "url": page_url,
+                    "identifier": page_url.replace(
+                        "https://printprobdb.psc.edu/iiif//", ""
+                    ),
+                    "custom_tiles": [],
+                }
+            character_tile = {
+                "region_x": c["absolute_coords"]["x"],
+                "region_y": c["absolute_coords"]["y"],
+                "region_w": c["absolute_coords"]["w"],
+                "region_h": c["absolute_coords"]["h"],
+            }
+            thumbnail_tile = {
+                "region_x": c["absolute_coords"]["x"],
+                "region_y": c["absolute_coords"]["y"],
+                "region_w": c["absolute_coords"]["w"],
+                "region_h": c["absolute_coords"]["h"],
+                "size_w": 500,
+            }
+            all_images[identifier]["custom_tiles"].append(character_tile)
+            all_images[identifier]["custom_tiles"].append(thumbnail_tile)
+        list_images = [v for k, v in all_images.items()]
+        with open(f"serialized_json/converter.json", "w") as dumpfile:
+            json.dump(list_images, dumpfile, cls=UUIDEncoder, indent=2)
