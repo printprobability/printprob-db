70d31b9eec483c7221bf269bdba142680f2e3d34
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2022-03-26 10:37:46 -0400

    try to eliminate iiif jitter by removing columns

--- vue/src/components/Characters/CharacterDetail.vue
+++ vue/src/components/Characters/CharacterDetail.vue
@@ -1,35 +1,39 @@
 <template>
-  <b-row v-if="!!character">
-    <b-col cols="3">
-      <b-card header="Character detail" class="m-2">
-        <p>
-          Book:
-          <router-link
-            :to="{
-              name: 'BookDetailView',
-              params: { id: this.character.book.id },
-            }"
-            >{{ this.character.book.label }}</router-link
-          >
-        </p>
-        <p>
-          Location: page {{ this.character.page.sequence }}, line
-          {{ this.character.line.sequence }}, position
-          {{ this.character.sequence }}
-        </p>
-        <p>
-          Assigned class: {{ this.character.character_class }} ({{
-            this.character.class_probability
-          }})
-        </p>
-        <p>Human character class: {{ this.character.human_character_class }}</p>
-        <p>
-          Character id:
-          <code>{{ this.character.id }}</code>
-        </p>
-      </b-card>
-    </b-col>
-    <b-col cols="9">
+  <b-container v-if="!!character">
+    <b-row>
+      <b-col cols="12">
+        <b-card header="Character detail" class="m-2">
+          <p>
+            Book:
+            <router-link
+              :to="{
+                name: 'BookDetailView',
+                params: { id: this.character.book.id },
+              }"
+              >{{ this.character.book.label }}</router-link
+            >
+          </p>
+          <p>
+            Location: page {{ this.character.page.sequence }}, line
+            {{ this.character.line.sequence }}, position
+            {{ this.character.sequence }}
+          </p>
+          <p>
+            Assigned class: {{ this.character.character_class }} ({{
+              this.character.class_probability
+            }})
+          </p>
+          <p>
+            Human character class: {{ this.character.human_character_class }}
+          </p>
+          <p>
+            Character id:
+            <code>{{ this.character.id }}</code>
+          </p>
+        </b-card>
+      </b-col>
+    </b-row>
+    <b-row>
       <div class="m-2">
         <AnnotatedImage
           :id="this.character.id"
@@ -37,8 +41,8 @@
           :overlay="annotation"
         />
       </div>
-    </b-col>
-  </b-row>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
