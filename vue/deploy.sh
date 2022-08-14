0be0475b73522c4358e8dee5122b4c7dc9645df6
Sriram Viswanathan <sriram.vish@gmail.com>
2022-08-14 15:28:06 +0530

    Update rsync script to make sure everyone in the user group can update the front-end artifacts

--- vue/deploy.sh
+++ vue/deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+## Build front end
 npm run build
-rsync -avh --recursive --perms --chmod=Du=rwx,Dg=rwx,Do=rx,Fu=rw,Fg=rw,Fo=r dist/ vm01
-2.bridges2.psc.edu:/data/vue/
+
+## Either get SSH_USER environment variable for the username or default to 'sviswana' username
+export SSH_USER=${SSH_USER:-sviswana}
+
+## Rsync the `dist` directory contents
+# Make sure the 'group' gets read/write permissions
+# '--perms' is necessary for '--chmod' to work
+rsync -avh --recursive --perms --no-times --chmod=Du=rwx,Dg=rwx,Do=rx,Fu=rw,Fg=rw,Fo=r --delete dist/ ${SSH_USER}@vm012.bridges2.psc.edu:/data/vue/
