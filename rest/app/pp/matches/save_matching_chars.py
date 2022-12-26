393569fb70098133598b79f17a4fd3c9169c3cfa
Sriram Viswanathan <sriram.vish@gmail.com>
2022-12-26 19:13:00 +0530

    Update logic for checking existing

--- rest/app/pp/matches/save_matching_chars.py
+++ rest/app/pp/matches/save_matching_chars.py
@@ -7,16 +7,14 @@ def save_matched_characters_in_db(book, matched_chars):
         if matched_char is not None and matched_char['query'] is not None:
             try:
                 query = models.Character.objects.get(id=matched_char['query'])
-                logging.info({"Found existing match:": models.CharacterMatch.objects.filter(book=book, query=query)})
-                exists = models.CharacterMatch.objects.filter(book=book, query=query).exists()
-                if exists:
+                existing = models.CharacterMatch.objects.filter(book=book, query=query).first()
+                if existing is None:
                     if matched_char['match'] is None: # nothing to save
                         continue
                     logging.info("Saving new match")
                     match = models.Character.objects.get(id=matched_char['match'])
                     models.CharacterMatch.objects.create(book=book, query=query, match=match)
                 else:
-                    existing = models.CharacterMatch.objects.filter(book=book, query=query)
                     logging.info({"Existing:": existing})
                     if matched_char['match'] is None:
                         logging.info("Deleting existing match")
