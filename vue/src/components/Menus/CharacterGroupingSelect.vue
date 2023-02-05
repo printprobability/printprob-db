61995179ddeb5f61a519c4f36f44bbbde72df220
Sriram Viswanathan <sriram.vish@gmail.com>
2023-02-04 22:02:18 -0500

    Change mount to asyn computed property

--- vue/src/components/Menus/CharacterGroupingSelect.vue
+++ vue/src/components/Menus/CharacterGroupingSelect.vue
@@ -4,7 +4,7 @@
       class="auto_select"
       :minMatchingChars="min_matching_chars"
       :max-matches="max_matches"
-      v-if="character_groupings.length"
+      v-if="!!character_groupings"
       :data="character_groupings"
       :serializer="(item) => item.text"
       :value="value"
@@ -34,31 +34,32 @@ export default {
   },
   data() {
     return {
-      character_groupings: [],
       min_matching_chars: 1,
       max_matches: 1000,
     }
   },
-  mounted() {
-    HTTP.get('/character_groupings/', { params: { limit: 200 } }).then(
-      (response) => {
-        const result = _.sortBy(
-          response.data.results.map((x) => ({
-            value: x.id,
-            text: x.label,
-          })),
-          'text'
-        )
-        this.character_groupings = !!this.excludedCharacterGroup
-          ? result.filter(
-              (option) => option.value !== this.excludedCharacterGroup
-            )
-          : result
-      },
-      (error) => {
-        console.log(error)
-      }
-    )
+  asyncComputed: {
+    character_groupings() {
+      return HTTP.get('/character_groupings/', { params: { limit: 200 } }).then(
+        (response) => {
+          const result = _.sortBy(
+            response.data.results.map((x) => ({
+              value: x.id,
+              text: x.label,
+            })),
+            'text'
+          )
+          return !!this.excludedCharacterGroup
+            ? result.filter(
+                (option) => option.value !== this.excludedCharacterGroup
+              )
+            : result
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+    },
   },
 }
 </script>
